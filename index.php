647cc77976307ce2d507bccb86584d39a5d5eaf3
ClemensJongma <clemensjongma@outlook.com>
2019-06-26 11:55:35 +0200

    input and result on 1 screen

--- index.php
+++ index.php
@@ -10,101 +10,67 @@
 
 <body>
 <div class="parallax">
-<div class="container">
-    <!-- <input type="text" id="lengte"=> is lengte</input><br>
-    <input type="text" id="breedte"=> is breedte</input><br>
-    <input type="text" id="diepte"=> is diepte</input><br>
-    <input type="text" id="rand"=> is rand</input><br>
-    <input type="text" id="marge"=> is marge</input><br> -->
-
-    <div class="row">
-              <div class="col-75">
-                <label for="lng">Hoe lang wordt uw zwembad?</label>
-              </div>
-              <div class="col-25">
-                <input type="text" id="lengte" placeholder="lengte in meters..">
-              </div>
-        </div>  
+        <div class="container">
+    
+          <div class="column">  
+            <div class="row">
+                      <div class="col-75">
+                        <label for="lng">Hoe lang wordt uw zwembad?</label>
+                      </div>
+                      <div class="col-25">
+                        <input type="text" id="lengte" placeholder="lengte in meters..">
+                      </div>
+            </div>  
 
 
-        <div class="row">
-              <div class="col-75">
-                <label for="brd">Hoe breed wordt uw zwembad?</label>
-              </div>
-              <div class="col-25">
-                <input type="text" id="breedte" placeholder="breedte in meters..">
-              </div>
-        </div>
+            <div class="row">
+                  <div class="col-75">
+                    <label for="brd">Hoe breed wordt uw zwembad?</label>
+                  </div>
+                  <div class="col-25">
+                    <input type="text" id="breedte" placeholder="breedte in meters..">
+                  </div>
+            </div>
 
 
-        <div class="row">
-              <div class="col-75">
-                <label for="dp">Hoe diep wordt uw zwembad?</label>
-              </div>
-              <div class="col-25">
-                <input type="text" id="diepte" placeholder="diepte in meters..">
-              </div>
-        </div>
+            <div class="row">
+                  <div class="col-75">
+                    <label for="dp">Hoe diep wordt uw zwembad?</label>
+                  </div>
+                  <div class="col-25">
+                    <input type="text" id="diepte" placeholder="diepte in meters..">
+                  </div>
+            </div>
+                
+
+            <div class="row">
+                  <div class="col-75">
+                    <label for="rnd">Hoe groot wordt de rand om het zwembad?</label>
+                  </div>
+                  <div class="col-25">
+                    <input type="text" id="rand" placeholder="rand in meters..">
+                  </div>
+            </div>
             
-
-        <div class="row">
-              <div class="col-75">
-                <label for="rnd">Hoe groot wordt de rand om het zwembad?</label>
-              </div>
-              <div class="col-25">
-                <input type="text" id="rand" placeholder="rand in meters..">
-              </div>
-        </div>
-        
-        <!-- <div class="row"> -->
-              <div class="col-75">
-                <label for="mrg">Wat wordt de afstand van de bovenkant tot het waterniveau?</label>
-              </div>
-              <div class="col-25">
-                <input type="text" id="marge" placeholder="afstand in meters..">
-              </div>
-        
+            <div class="row">
+                  <div class="col-75">
+                    <label for="mrg">Wat wordt de afstand van de bovenkant tot het waterniveau?</label>
+                  </div>
+                  <div class="col-25">
+                    <input type="text" id="marge" placeholder="afstand in meters..">
+                  </div>
+            </div>
 
 
-    <button onclick="toonResultaat()">Laat zien!</button>
 
-</div>   
-    <!-- <p>Resultaat breedte =</p>
-    <p id="demo"></p> -->
-<br>
-<br>
-    <!-- <div class="container">
-    <p>Plattegrond</p>
-    <canvas id="plattegrond" width="600" height="600"
-    style="border:1px solid #c3c3c3;">
-    Your browser does not support the canvas element.
-    </canvas>
-    </div>
-    <br>
-
-<br>
-<div class="container">
-<p>Lengtedoorsnede</p>
-<canvas id="zijaanzicht" width="600" height="600"
-style="border:1px solid #c3c3c3;">
-Your browser does not support the canvas element.
-</canvas>
-
-</div>
-<br>
-<br>
-
-<div class="container">
-<p>Dwarsdoorsnede </p>
-<canvas id="vooraanzicht" width="600" height="600"
-style="border:1px solid #c3c3c3;">
-Your browser does not support the canvas element.
-</canvas>
-
-</div>
-<br>
+            <button onclick="toonResultaat()">Laat zien!</button>
+    
+        </div>   
+    
+<!-- <br>
 <br> -->
-
+    
+<div class="column">
 <div class="container">
 <p>Opengewerkte isometrische projectie </p>
 <canvas id="isomproj" width="600" height="600"
